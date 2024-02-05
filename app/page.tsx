4383b92446419fbfe5c2a28fa681480c06174f48
Hope Warren <hopevaughnwarren@gmail.com>
2024-02-05 12:41:01 -0800

    add about, faq, and hero components and add them all to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import { ModeToggle } from '@/components/ModeToggle';
+import HeroSection from '@/components/HeroSection';
+import AboutSection from '@/components/AboutSection';
+import ContactSection from '@/components/ContactSection';
 export default function Home() {
   return (
     <>
-      <ModeToggle />
-      <h1>LandingPage</h1>
+      <section className="">
+        <HeroSection />
+      </section>
+      <section>
+        <AboutSection />
+      </section>
+      <section>
+        <ContactSection />
+      </section>
     </>
   );
 }
