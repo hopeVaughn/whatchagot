9bfd8077211a6b22edca7808841e7dd4f7c3fb4e
Hope Warren <hopevaughnwarren@gmail.com>
2024-02-12 11:21:19 -0800

    nuked this project for clarity

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react';
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return;
+  (
+    <div>Navbar</div>
+  );
+};
+
+export default Navbar
---
+++ lib/nav.ts
@@ -0,0 +1,8 @@
+const navigation = [
+  { name: 'Pricing', href: '#' },
+  { name: 'FAQ', href: '#' },
+  { name: 'Community CookBook', href: '#' },
+  { name: 'Contact Us', href: '#' },
+];
+
+export default navigation;
