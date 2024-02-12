0e7ea23c741ef79952b60fdc703a28fbdd8bd5f7
Hope Warren <hopevaughnwarren@gmail.com>
2024-02-12 13:27:54 -0800

    update navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,38 +5,40 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    // Fixed positioning on the left, with full height on small screens and vertically centered on lg and md screens
     <nav className="fixed top-0 left-0 lg:top-1/2 md:top-1/2 lg:-translate-y-1/2 md:-translate-y-1/2 lg:h-[70%] md:h-[70%] sm:h-full w-16 z-10 rounded-r-2xl">
-      {/* // Full height container with flexible box layout, vertically and horizontally centered items */}
-      <div className="flex flex-col  h-full bg-gray-700  rounded-r-2xl">
-        {/* // Links with border dividers */}
-        <Link href="#home" className="w-full flex-grow">
-          <div className="flex items-center justify-center w-full h-16  hover:bg-gray-600">
-            <Home />
-          </div>
-        </Link>
-        <Link href="#community" className="w-full flex-grow">
-          <div className="flex items-center justify-center w-full h-16 hover:bg-gray-600">
-            <BookOpenText />
-          </div>
-        </Link>
-        <Link href="#pricing" className="w-full flex-grow">
-          <div className="flex items-center justify-center w-full h-16 hover:bg-gray-600">
-            <CircleDollarSign />
-          </div>
-        </Link>
-        <Link href="#faq" className="w-full flex-grow">
-          <div className="flex items-center justify-center w-full h-16 hover:bg-gray-600">
-            <HelpCircle />
-          </div>
-        </Link>
-        <Link href="#contact" className="w-full flex-grow">
-          <div className="flex items-center justify-center w-full h-16  hover:bg-gray-600">
-            <Mail />
-          </div>
-        </Link>
-        <Link href="#contact" className="w-full flex-grow">
-          <div className="flex items-center justify-center w-full h-16 border-t border-gray-600 hover:bg-gray-600">
+      <div className="flex flex-col justify-between h-full bg-gray-700 rounded-r-2xl">
+        {/* All links except the last one will grow to fill the space evenly */}
+        <div className="flex flex-col flex-grow">
+          <Link href="#home" className="w-full flex-1">
+            <div className="flex items-center justify-center w-full h-full rounded-r-2xl hover:bg-gray-600">
+              <Home />
+            </div>
+          </Link>
+          <Link href="#community" className="w-full flex-1">
+            <div className="flex items-center justify-center w-full h-full hover:bg-gray-600">
+              <BookOpenText />
+            </div>
+          </Link>
+          <Link href="#pricing" className="w-full flex-1">
+            <div className="flex items-center justify-center w-full h-full hover:bg-gray-600">
+              <CircleDollarSign />
+            </div>
+          </Link>
+          <Link href="#faq" className="w-full flex-1">
+            <div className="flex items-center justify-center w-full h-full hover:bg-gray-600">
+              <HelpCircle />
+            </div>
+          </Link>
+          <Link href="#contact" className="w-full flex-1">
+            <div className="flex items-center justify-center w-full h-full hover:bg-gray-600">
+              <Mail />
+            </div>
+          </Link>
+        </div>
+
+        {/* This link will be pushed to the bottom */}
+        <Link href="#profile" className="w-full flex-1">
+          <div className="flex items-center justify-center w-full h-full rounded-r-2xl hover:bg-gray-600">
             <User />
           </div>
         </Link>
